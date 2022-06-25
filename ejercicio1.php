3d193c4911eec025f926900d82203d25e622109f
Ezequiel <ezequielta63@gmail.com>
2022-06-24 22:06:19 -0500

    se agregaron todos

---
+++ ejercicio1.php
@@ -0,0 +1,12 @@
+<?php
+//Estas son instrucciones de impresiones
+/*
+Comentarios largos
+*/
+
+echo "Este imprime solo textos";
+
+print_r("Este imprime cualquier valor");
+
+
+?>
---
+++ ejercicio2.php
@@ -0,0 +1,32 @@
+<?php
+//Recibe info del formulario html método post
+//Si existe valor imprimelo 
+if($_POST){
+$nombre=$_POST['txtNombre'];
+echo "Hola".$nombre;
+}
+//Si no hay valor del agrega esto
+else{
+    echo "Agrega Dato";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="ejercicio2.php" method="post">
+    Nombre:
+    <input type="text" name="txtNombre">
+    <br>
+    <input type="submit"value="Enviar">
+</form>
+
+    
+</body>
+</html>
